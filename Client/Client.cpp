41be1e8deaaf629a9bad22e03a51354a6c385788
SliverCicada <68686285+SliverCicada@users.noreply.github.com>
2021-09-01 16:11:01 +0800

    update 0x02

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,21 +1,62 @@
 ﻿#include "../header/client.h"
+#include <future>
+#include <iostream>
+
+void func(sol::state& lua)
+{
+	static auto count = 0;
+	static auto script = R"(
+co = coroutine.create(
+function()
+local x, y = 200, 200
+local start_r = 100
+local step = 20
+	for i = 1, 10 do
+		circle(x, y, start_r)
+		start_r = start_r + step
+		coroutine.yield()
+	end
+end
+)
+)";
+	static auto call = R"(
+coroutine.resume(co)
+return coroutine.status(co)
+)";
+
+	if (count >= 0) {
+		if (count == 0)
+			lua.script(script);
+
+		std::string s = lua.script(call);
+		std::cout << s << std::endl;
+		
+		count++;
+	}
+
+}
+
 
 int main()
 {
+
+	const char* version = hv_compile_version();
+	char run_dir[MAX_PATH] = { 0 };
+	get_run_dir(run_dir, sizeof(run_dir));
+
+	std::cout << version << std::endl;
+	std::cout << run_dir << std::endl;
     std::cout << "Hello World!\n";
 
-	//initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
-	//circle(200, 200, 100);	// 画圆，圆心(200, 200)，半径 100
-	//_getch();				// 按任意键继续
-	//closegraph();			// 关闭绘图窗口
-	//_getch();				// 按任意键继续
-
 	sol::state lua;
-	int x = 0;
+	int z = 0;
 	lua.open_libraries(sol::lib::base, sol::lib::package);
-	lua.set_function("beep", [&x] { ++x; });
-	lua.script("beep() print(\"hello\")");
-	assert(x == 1);
+	lua.open_libraries();
+	lua.set_function("circle", [](int x, int y, int r) { circle(x, y, r); });
+	lua.script("print(type(coroutine))");
+	std::cout << lua.memory_used() << std::endl;
+	_getch();
+	
 
 	json j_no_init_list = json::array();
 	json j_empty_init_list = json::array({});
@@ -27,6 +68,43 @@ int main()
 	std::cout << j_empty_init_list << '\n';
 	std::cout << j_nonempty_init_list << '\n';
 	std::cout << j_list_of_pairs << '\n';
+
+	srand((unsigned)time(NULL));
+
+	// 初始化图形模式
+	//initgraph(640, 480);
+
+	//int  x, y;
+	//char c;
+
+	//settextstyle(16, 8, _T("Courier"));	// 设置字体
+
+	//// 设置颜色
+	//settextcolor(GREEN);
+	//setlinecolor(BLACK);
+
+	//for (int i = 0; i <= 479; i++)
+	//{
+	//	// 在随机位置显示三个随机字母
+	//	for (int j = 0; j < 3; j++)
+	//	{
+	//		x = (rand() % 80) * 8;
+	//		y = (rand() % 20) * 24;
+	//		c = (rand() % 26) + 65;
+	//		outtextxy(x, y, c);
+	//	}
+
+	//	// 画线擦掉一个像素行
+	//	line(0, i, 639, i);
+
+	//	Sleep(10);					// 延时
+	//	if (i >= 479)	i = -1;
+	//	if (_kbhit())	break;		// 按任意键退出
+	//}
+
+	//// 关闭图形模式
+	//closegraph();
+	return 0;
 	
 
     std::cout << "Hello World!\n";
--- header/client.h
+++ header/client.h
@@ -1,12 +1,16 @@
-
-
 #include "../header/sol/config.hpp"
 #include "../header/sol/forward.hpp"
 #include "../header/sol/sol.hpp"
 
 #include "../header/json/json.hpp"
 
+#include "../header/hv/hv.h"
+
+
 #include <conio.h>
 #include <cstdlib>
+#include <ctime>
+
 #include <easyx.h>
+
 using json = nlohmann::json;
